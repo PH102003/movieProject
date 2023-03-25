f64953b7a093d37ce9d44b749404955ffa5e6776
Pedro Henrique <pedroh.rj2003@hotmail.com>
2023-03-25 18:07:13 -0300

    adiçao

---
+++ Filme.java
@@ -0,0 +1,15 @@
+public class Filme{
+    String nome;
+    int duracao;
+    String sinopse;
+    float valor;
+
+    public Filme( String nome, String sinopse, int duracao, float valor){
+        this.nome = nome;
+        this.sinopse = sinopse;
+        this.duracao = duracao;
+        this.valor = valor;
+    }
+
+
+}
